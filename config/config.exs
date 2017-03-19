e4fe4f916f2f75a9ffdb247675eb0c5dddadf07b
Александр Фомин <ll.wg.bin@gmail.com>
2017-03-19 14:35:43 +0300

    default amqp settings

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,11 @@ use Mix.Config
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:consumer, :tag, :redelivered]
+
+config :capsuline, Coney.AMQPConnection, [
+  adapter: Coney.RabbitConnection,
+  settings: %{
+    url: "amqp://guest:guest@localhost",
+    timeout: 1000
+  }
+]
