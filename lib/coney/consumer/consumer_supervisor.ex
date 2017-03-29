d088afc0bd9dab34c7410e8d176ac9fcfaca032a
Александр Фомин <ll.wg.bin@gmail.com>
2017-03-29 14:45:49 +0300

    consumer supervisor

---
+++ lib/coney/consumer/consumer_supervisor.ex
@@ -0,0 +1,23 @@
+defmodule Coney.ConsumerSupervisor do
+  require Logger
+
+  use Supervisor
+
+  @name __MODULE__
+
+  def start_link do
+    Supervisor.start_link(@name, [], name: @name)
+  end
+
+  def init([]) do
+    children = [
+      worker(Coney.ConsumerServer, [])
+    ]
+
+    supervise(children, strategy: :simple_one_for_one)
+  end
+
+  def start_consumer(consumer, connection) do
+    Supervisor.start_child(@name, [consumer, connection])
+  end
+end
