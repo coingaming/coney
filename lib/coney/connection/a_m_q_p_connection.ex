745464a041494ee9d50e0de724273ca1f947aee2
Александр Фомин <ll.wg.bin@gmail.com>
2017-08-26 09:18:06 +0300

    fix warnings

--- lib/coney/connection/a_m_q_p_connection.ex
+++ lib/coney/connection/a_m_q_p_connection.ex
@@ -6,8 +6,6 @@ defmodule Coney.AMQPConnection do
   @callback create_channel(conn) :: chan
   @callback subscribe(chan, pid, atom) :: tuple
   @callback respond_to(chan, {atom, String.t}) :: any
-  @callback publish(String.t, String.t) :: any
-  @callback publish(String.t, String.t, String.t) :: any
   @callback publish(chan, String.t, String.t, String.t) :: any
   @callback confirm(chan, String.t) :: any
   @callback reject(chan, String.t, nonempty_list) :: any
