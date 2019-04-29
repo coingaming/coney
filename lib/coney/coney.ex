509190dc7dae07ed6837824577158ceaaabbe00b
Aleksandr Fomin <ll.wg.bin@gmail.com>
2019-04-29 13:00:30 +0300

    fix specs

--- lib/coney/coney.ex
+++ lib/coney/coney.ex
@@ -1,12 +1,12 @@
 defmodule Coney do
   alias Coney.{ConnectionServer, HealthCheck.ConnectionRegistry}
 
-  @spec publish(String.t(), String.t()) :: :published | {:error, :no_connected_servers}
+  @spec publish(String.t(), binary()) :: :published | {:error, :no_connected_servers}
   def publish(exchange_name, message) do
     ConnectionServer.publish(exchange_name, message)
   end
 
-  @spec publish(String.t(), String.t(), String.t()) :: :published | {:error, :no_connected_servers}
+  @spec publish(String.t(), String.t(), binary()) :: :published | {:error, :no_connected_servers}
   def publish(exchange_name, routing_key, message) do
     ConnectionServer.publish(exchange_name, routing_key, message)
   end
