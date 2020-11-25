dd6244fea70254eae1d7213f6a7f35a6fee7c134
Aleksandr Fomin <ll.wg.bin@gmail.com>
2020-11-25 20:51:50 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Coney.Mixfile do
   def project do
     [
       app: :coney,
-      version: "3.0.0-rc.2",
+      version: "3.0.0-rc.3",
       elixir: ">= 1.6.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
