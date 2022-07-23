3d5cf6ddf994c77d7c4f959fd5de38521e15daaf
Sasha Fomin <ll.wg.bin@gmail.com>
2022-07-23 08:25:09 +0200

    Bump version to 3.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+
+## [3.0.1]
+
+- Updated dependencies (`amqp`) #16
+
 ## [3.0.0]
 
 ### Changes
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Coney.Mixfile do
   def project do
     [
       app: :coney,
-      version: "3.0.0",
+      version: "3.0.1",
       elixir: ">= 1.10.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
